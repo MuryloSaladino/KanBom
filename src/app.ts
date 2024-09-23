f8126e70c5545b058f382e59efeace053d800577
MuryloSaladino <murylosaladino@gmail.com>
2024-09-23 10:03:20 -0300

    get notifications

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import userRouter from "./routes/users.routes";
 import handleError from "./middlewares/handleError.middleware";
 import loginRouter from "./routes/login.routes";
 import teamRouter from "./routes/teams.routes";
+import notificationsRouter from "./routes/notification.routes";
 
 
 const app = express();
@@ -17,6 +18,7 @@ app.use(cors());
 app.use("/api/users", userRouter);
 app.use("/api/login", loginRouter);
 app.use("/api/teams", teamRouter);
+app.use("/api/notifications", notificationsRouter);
 
 app.use(handleError);
 
