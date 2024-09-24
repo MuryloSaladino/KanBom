28f26b21c7f6e7b69e9931552a8f912b9c4ec7f8
janeckiisa <janeckiisabelle@gmail.com>
2024-09-24 14:06:14 -0300

    Starting 'deleteProjectService' method.

--- src/services/projects.services.ts
+++ src/services/projects.services.ts
@@ -5,35 +5,52 @@ import User from "../entities/User.entity";
 import { stringToRole } from "../enums/Role";
 import AppError from "../errors";
 
-export async function createProjectService( name: string, thumbnail: string ) : Promise<Project>
-{
+export async function createProjectService(name: string, thumbnail: string): Promise<Project> {
     const projectRepo = AppDataSource.getRepository(Project);
 
-    const project = projectRepo.create({ name: name, thumbnail: thumbnail});
+    const project = projectRepo.create({ name: name, thumbnail: thumbnail });
 
-    return await projectRepo.save(project); 
+    return await projectRepo.save(project);
 }
 
-export async function addUserToProject(userId: string, projectId: string, roleString: string)
-{
+export async function addUserToProjectService(userId: string, projectId: string, roleString: string) {
     const userRepo = AppDataSource.getRepository(User);
     const participantRepo = AppDataSource.getRepository(Participant);
     const projectRepo = AppDataSource.getRepository(Project);
 
     const user = await userRepo.findOneBy({ id: userId });
-    if(!user) throw new AppError("User not found", 404);
+    if (!user) throw new AppError("User not found", 404);
 
     const project = await projectRepo.findOneBy({ id: projectId });
-    if(!project) throw new AppError("Project not found", 404);
+    if (!project) throw new AppError("Project not found", 404);
 
     const role = stringToRole(roleString);
 
     const participant = participantRepo.create
-    ({ 
-        user: user, 
-        project: project, 
-        role: role 
-    })
+        ({
+            user: user,
+            project: project,
+            role: role
+        })
 
     return await participantRepo.save(participant);
+}
+
+export async function deleteProjectService(id: string): Promise<void> {
+    const repo = AppDataSource.getRepository(Project);
+
+    if (!await repo.exists({ where: { id } }))
+        throw new AppError("Project not found", 404);
+
+    //verificar token:
+    /*
+        - pegar o id do usuário (req.locals.userId)
+        - pegar o id do projeto atual
+        - Procurar no projeto participantes com esse id
+        - Verificar se o Role é "Owner"
+            - não tem participante => 403 - unauthorized
+            - não é owner => 403 => unauthorized
+        - Soft Delete 
+    */
+    await repo.softDelete({ id });
 }
