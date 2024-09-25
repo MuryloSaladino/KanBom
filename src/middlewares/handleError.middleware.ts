47db72112a85f34f3ee97ed62289386e711a1340
MuryloSaladino <murylosaladino@gmail.com>
2024-09-25 10:29:40 -0300

    query failed error

--- src/middlewares/handleError.middleware.ts
+++ src/middlewares/handleError.middleware.ts
@@ -1,16 +1,19 @@
 import { NextFunction, Request, Response } from "express"
 import AppError from "../errors"
 import { ZodError } from "zod"
+import { QueryFailedError } from "typeorm";
 
 export default function handleError(err:Error, _req:Request, res:Response, next:NextFunction) {
     switch (err.constructor) {
         case AppError:
             const appError = err as AppError;
-            return res.status(appError.statusCode).json({ message: err.message })
+            return res.status(appError.statusCode).json({ message: err.message });
         case ZodError:
             const zodError = err as ZodError;
-            return res.status(400).json({ message: zodError.flatten().fieldErrors })
+            return res.status(400).json({ message: zodError.flatten().fieldErrors });
+        case QueryFailedError:
+            return res.status(400).json({ message: "Data could not be saved" })
         default:
-            return res.status(500).json({ message: "Internal Server Error" })
+            return res.status(500).json({ message: "Internal Server Error" });
     }
 }
