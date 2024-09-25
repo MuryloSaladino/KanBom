c224902fef12fb6e848749ea794ce4f6f9873248
MuryloSaladino <murylosaladino@gmail.com>
2024-09-25 08:33:52 -0300

    validate body team update

--- src/routes/teams.routes.ts
+++ src/routes/teams.routes.ts
@@ -3,14 +3,14 @@ import authenticate from "../middlewares/authenticate.middleware";
 import { createTeamController, deleteTeamController, getTeamController, getTeamsByUserController, updateTeamController } from "../controllers/teams.controllers";
 import { authorizeMember, authorizeTeamOwner } from "../middlewares/teams.middlewares";
 import validateBody from "../middlewares/validateBody.middleware";
-import { createTeamSchema } from "../schemas/teams.schemas";
+import { createTeamSchema, updateTeamSchema } from "../schemas/teams.schemas";
 
 const teamRouter = Router();
 
 teamRouter.post("", authenticate, validateBody(createTeamSchema), createTeamController);
 teamRouter.get("/:teamId", authenticate, authorizeMember, getTeamController);
 teamRouter.get("/users/:userId", authenticate, getTeamsByUserController);
-teamRouter.patch("/:teamId", authenticate, authorizeTeamOwner, updateTeamController);
+teamRouter.patch("/:teamId", authenticate, authorizeTeamOwner, validateBody(updateTeamSchema), updateTeamController);
 teamRouter.delete("/:teamId", authenticate, authorizeTeamOwner, deleteTeamController);
 
 export default teamRouter;
--- src/schemas/teams.schemas.ts
+++ src/schemas/teams.schemas.ts
@@ -3,3 +3,7 @@ import { z } from "zod";
 export const createTeamSchema = z.object({
     name: z.string().min(3).max(50),
 }).strict()
+
+export const updateTeamSchema = z.object({
+    name: z.string().min(3).max(50).optional(),
+}).strict()
