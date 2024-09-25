bc5e97c9ae67f2aefbb8c959fe626b69eb4d1340
MuryloSaladino <murylosaladino@gmail.com>
2024-09-25 08:26:03 -0300

    fix date on schemas

--- src/schemas/users.schemas.ts
+++ src/schemas/users.schemas.ts
@@ -4,16 +4,16 @@ export const createUserSchema = z.object({
     email: z.string().email(),
     password: z.string().min(8),
     details: z.object({
-        birthdate: z.date(),
+        birthdate: z.string().date(),
         firstName: z.string().min(2).max(50),
         lastName: z.string().min(2).max(255),
-        profilePicture: z.string().max(255)
+        profilePicture: z.string().max(255).nullable()
     })
 }).strict();
 
 export const updateUserSchema = z.object({
-    birthdate: z.date().optional(),
+    birthdate: z.string().date().optional(),
     firstName: z.string().min(2).max(50).optional(),
     lastName: z.string().min(2).max(255).optional(),
-    profilePicture: z.string().max(255).optional()
+    profilePicture: z.string().max(255).nullable().optional()
 }).strict();
