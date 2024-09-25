f083ec7d617c38d49d60fc0afc62769098d22328
MuryloSaladino <murylosaladino@gmail.com>
2024-09-25 08:32:43 -0300

    validate body team creation

--- src/routes/teams.routes.ts
+++ src/routes/teams.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import authenticate from "../middlewares/authenticate.middleware";
 import { createTeamController, deleteTeamController, getTeamController, getTeamsByUserController, updateTeamController } from "../controllers/teams.controllers";
 import { authorizeMember, authorizeTeamOwner } from "../middlewares/teams.middlewares";
+import validateBody from "../middlewares/validateBody.middleware";
+import { createTeamSchema } from "../schemas/teams.schemas";
 
 const teamRouter = Router();
 
-teamRouter.post("", authenticate, createTeamController);
+teamRouter.post("", authenticate, validateBody(createTeamSchema), createTeamController);
 teamRouter.get("/:teamId", authenticate, authorizeMember, getTeamController);
 teamRouter.get("/users/:userId", authenticate, getTeamsByUserController);
 teamRouter.patch("/:teamId", authenticate, authorizeTeamOwner, updateTeamController);
