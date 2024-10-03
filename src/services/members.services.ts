90a1ba9bae388f8e759bab25c072a62b95093c9a
NiltonMeira <niltonprudys@gmail@gmail.com>
2024-10-03 13:50:36 -0300

    refact query

--- src/services/members.services.ts
+++ src/services/members.services.ts
@@ -48,21 +48,10 @@ export async function acceptTeamInvitationService(teamId:string, userId:string) 
 }
 
 export async function getTeamMembersService(teamId:string) {
-    
-    const repo = AppDataSource.getRepository(Team);
-
-    const team = await repo.findOne({ where: { id: teamId }, relations: { members: true } });
-    if(!team) throw new AppError("Team not found", 404);
-
-    const query = await AppDataSource
-        .getRepository(User)
-        .createQueryBuilder("u")
-        .leftJoinAndSelect("u.details", "ud")
-        .innerJoin("members", "m", "m.userId = u.id")
-        .innerJoin("teams", "t", "m.teamId = t.id")
-        .where("t.id = :teamId", { teamId })
-        .getMany();
-    return query.map(user => user.hideFields())
+    return await AppDataSource.getRepository(User).find({
+        where: { memberIn: { teamId } },
+        relations: { details: true }
+    });
 }
 
 export async function removeMemberService(teamId:string, userId:string) {
