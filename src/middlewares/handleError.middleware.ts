1b4767b9d8a63a3663f41d16ef2fab59d1275957
MuryloSaladino <murylosaladino@gmail.com>
2024-10-04 11:01:35 -0300

    readability

--- src/middlewares/handleError.middleware.ts
+++ src/middlewares/handleError.middleware.ts
@@ -5,14 +5,18 @@ import { QueryFailedError } from "typeorm";
 
 export default function handleError(err:Error, _req:Request, res:Response, next:NextFunction) {
     switch (err.constructor) {
+        
         case AppError:
             const appError = err as AppError;
             return res.status(appError.statusCode).json({ message: err.message });
+
         case ZodError:
             const zodError = err as ZodError;
             return res.status(400).json({ message: zodError.flatten().fieldErrors });
+
         case QueryFailedError:
-            return res.status(400).json({ message: "Data could not be saved" })
+            return res.status(400).json({ message: "Data could not be saved" });
+
         default:
             return res.status(500).json({ message: "Internal Server Error" });
     }
