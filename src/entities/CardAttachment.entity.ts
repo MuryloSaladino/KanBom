e18f93aa82d2e95e6bac29f6abad30fe973ee438
MuryloSaladino <murylosaladino@gmail.com>
2024-10-30 09:08:20 -0300

    fix pk

--- src/entities/CardAttachment.entity.ts
+++ src/entities/CardAttachment.entity.ts
@@ -1,10 +1,11 @@
 import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
 import Card from "./Card.entity";
+import BaseEntity from "./common/BaseEntity.entity";
 
 export type TAttachment = "file" | "photo" | "link";
 
 @Entity("card_attachments")
-export default class CardAttachment {
+export default class CardAttachment extends BaseEntity {
 
     @Column()
     url?: string;
