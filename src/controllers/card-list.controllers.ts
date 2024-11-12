fbc3f4a628235d95c2e1fd7916a9f33e6a0c91c4
MuryloSaladino <murylosaladino@gmail.com>
2024-11-12 10:25:21 -0300

    integrated card lists

--- src/controllers/card-list.controllers.ts
+++ src/controllers/card-list.controllers.ts
@@ -1,23 +1,51 @@
 import { Request, Response } from "express";
-import { createCardListService, deleteCardListService, getCardListsByBoardService, updateCardListService } from "../services/cardList.services";
+import CardListsService from "../services/cardList.services";
+import { Controller, ControllerMiddlewares, HttpMethod, Route, RouteMiddlewares } from "../decorators/api.decorators";
+import authenticate from "../middlewares/authenticate.middleware";
+import { authorizeByBoardRole } from "../middlewares/boards.middlewares";
 
 
-export async function createCardListController(req:Request, res:Response) {
-    const cardList = await createCardListService(req.params.boardId, req.body);
-    return res.status(201).json(cardList);
-}
+@Controller("/card-lists")
+@ControllerMiddlewares([authenticate])
+export default class CardListsController {
 
-export async function getCardListsByBoardController(req:Request, res:Response) {
-    const cardLists = await getCardListsByBoardService(req.params.boardId);
-    return res.status(200).json(cardLists);
-}
+    private service = new CardListsService();
 
-export async function updateCardListController(req:Request, res:Response) {
-    const cardList = await updateCardListService(req.params.cardListId, req.body);
-    return res.status(200).json(cardList);
-}
+    @HttpMethod("post")
+    @Route("/boards/:boardId")
+    @RouteMiddlewares([authorizeByBoardRole(["Editor", "Owner"])])
+    public create = async (req:Request, res:Response) => {
+        const cardList = await this.service.create({
+            boardId: req.params.boardId, 
+            ...req.body
+        });
+        return res.status(201).json(cardList);
+    }
 
-export async function deleteCardListController(req:Request, res:Response) {
-    await deleteCardListService(req.params.cardListId);
-    return res.status(204).send();
+    @HttpMethod("get")
+    @Route("/boards/:boardId")
+    @RouteMiddlewares([authorizeByBoardRole(["Reader", "Editor", "Owner"])])
+    public getByBoard = async (req:Request, res:Response) => {
+        const cardLists = await this.service.findAll({
+            relations: { cards: true },
+            where: { boardId: req.params.boardId },
+        });
+        return res.status(200).json(cardLists);
+    }
+
+    @HttpMethod("put")
+    @Route("/:cardListId")
+    @RouteMiddlewares([authorizeByBoardRole(["Editor", "Owner"])])
+    public update = async (req:Request, res:Response) => {
+        const cardList = await this.service.update(req.params.cardListId, req.body);
+        return res.status(200).json(cardList);
+    }
+
+    @HttpMethod("delete")
+    @Route("/:cardListId")
+    @RouteMiddlewares([authorizeByBoardRole(["Editor", "Owner"])])
+    public delete = async (req:Request, res:Response) => {
+        await this.service.delete(req.params.cardListId);
+        return res.status(204).send();
+    }
 }
--- src/services/cardList.services.ts
+++ src/services/cardList.services.ts
@@ -1,38 +1,7 @@
-import AppDataSource from "../data-source";
-import Board from "../entities/Board.entity";
+import BaseService from "../common/base.services";
 import CardList from "../entities/CardList.entity";
-import AppError from "../errors";
-import { ICardListPayload, ICardListUpdate } from "../types/cards.types";
 
-export async function createCardListService(boardId:string, payload:ICardListPayload) {
-    
-    const board = await AppDataSource.getRepository(Board).findOneBy({ id: boardId });
-    if(!board) throw new AppError("Board not found", 404);
+export default class CardListsService extends BaseService<CardList> {
 
-    return await AppDataSource.getRepository(CardList).save({ ...payload, boardId });
-}
-
-export async function getCardListsByBoardService(boardId:string) {
-    return await AppDataSource.getRepository(CardList).find({
-        relations: { cards: true },
-        where: { boardId },
-    });
-}
-
-export async function updateCardListService(cardListId:string, payload:ICardListUpdate) {
-    const repo = AppDataSource.getRepository(CardList);
-
-    const list = await repo.findOneBy({ id: cardListId });
-    if(!list) throw new AppError("Card list not found", 404);
-
-    return await repo.save({ ...list, ...payload })
-}
-
-export async function deleteCardListService(cardListId:string) {
-    const repo = AppDataSource.getRepository(CardList);
-    
-    const list = await repo.findOneBy({ id: cardListId })
-    if(!list) throw new AppError("Card list not found", 404);
-
-    await AppDataSource.getRepository(CardList).softRemove(list);
+    public constructor() { super(CardList) }
 }
