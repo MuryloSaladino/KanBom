0e6d97653e6538a15c0ccde0f08100fb23f43daa
MuryloSaladino <murylosaladino@gmail.com>
2024-11-12 08:56:47 -0300

    workspaces integrated

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,11 @@
 import LoginController from "./login.controllers";
 import NotificationsController from "./notifications.controllers";
 import UsersController from "./users.controllers";
+import WorkspacesController from "./workspaces.controllers";
 
 export default [
     UsersController,
     LoginController,
     NotificationsController,
+    WorkspacesController,
 ]
--- src/controllers/workspaces.controllers.ts
+++ src/controllers/workspaces.controllers.ts
@@ -1,27 +1,68 @@
 import { Request, Response } from "express";
-import { createWorkspaceService, deleteWorkspaceService, getWorkspacesByUserService, getWorkspaceService, updateWorkspaceService } from "../services/workspaces.services";
+import WorkspacesService from "../services/workspaces.services";
+import { Controller, HttpMethod, Middlewares, Route } from "../decorators/api.decorators";
+import authenticate from "../middlewares/authenticate.middleware";
+import { authorizeMember, authorizeWorkspaceOwner } from "../middlewares/workspaces.middlewares";
+import validateBody from "../middlewares/validateBody.middleware";
+import { WorkspaceSchema } from "../schemas/workspaces.schemas";
 
-export async function createWorkspaceController(req:Request, res:Response) {
-    const workspace = await createWorkspaceService(res.locals.userId, req.body);
-    return res.status(201).json(workspace);
-}
+@Controller("/workspaces")
+export default class WorkspacesController {
 
-export async function getWorkspaceController(req:Request, res:Response) {
-    const workspace = await getWorkspaceService(req.params.workspaceId);
-    return res.status(200).json(workspace);
-}
+    private service = new WorkspacesService();
 
-export async function getWorkspacesByUserController(req:Request, res:Response) {
-    const workspaces = await getWorkspacesByUserService(req.params.userId);
-    return res.status(200).json(workspaces);
-}
+    @HttpMethod("post")
+    @Middlewares([authenticate, validateBody(WorkspaceSchema)])
+    create() {
+        return async (req:Request, res:Response) => {
+            const workspace = await this.service.create({
+                userId: res.locals.userId, 
+                ...req.body
+            });
+            return res.status(201).json(workspace);
+        }
+    }
 
-export async function updateWorkspaceController(req:Request, res:Response) {
-    const workspace = await updateWorkspaceService(req.params.workspaceId, req.body);
-    return res.status(200).json(workspace);
-}
+    @HttpMethod("get")
+    @Route("/:workspaceId")
+    @Middlewares([authenticate, authorizeMember])
+    get() {
+        return async (req:Request, res:Response) => {
+            const workspace = await this.service.findById(req.params.workspaceId);
+            return res.status(200).json(workspace);
+        }
+    }
 
-export async function deleteWorkspaceController(req:Request, res:Response) {
-    await deleteWorkspaceService(req.params.workspaceId);
-    return res.status(204).send();
+    @HttpMethod("get")
+    @Route("/users/:userId")
+    @Middlewares([authenticate])
+    getByUser() {
+        return async (req:Request, res:Response) => {
+            const workspaces = await this.service.findAll({
+                where: { members: { userId: req.params.userId } },
+                relations: { boards: true }
+            });
+            return res.status(200).json(workspaces);
+        }
+    }
+
+    @HttpMethod("put")
+    @Route("/:workspaceId")
+    @Middlewares([authenticate, authorizeWorkspaceOwner, validateBody(WorkspaceSchema)])
+    update() {
+        return async (req:Request, res:Response) => {
+            const workspace = await this.service.update(req.params.workspaceId, req.body);
+            return res.status(200).json(workspace);
+        }
+    }
+
+    @HttpMethod("delete")
+    @Route("/:workspaceId")
+    @Middlewares([authenticate, authorizeWorkspaceOwner])
+    delete() {
+        return async (req:Request, res:Response) => {
+            await this.service.delete(req.params.workspaceId);
+            return res.status(204).send();
+        }
+    }
 }
