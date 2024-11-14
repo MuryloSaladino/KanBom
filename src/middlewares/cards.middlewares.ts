126e8cbfb0fc2ddfe32be0d49cad3614eb4dc4d4
MuryloSaladino <murylosaladino@gmail.com>
2024-11-14 09:13:21 -0300

    authorize middleware

--- src/middlewares/cards.middlewares.ts
+++ src/middlewares/cards.middlewares.ts
@@ -1,7 +1,5 @@
 import { NextFunction, Request, Response } from "express"
 import AppDataSource from "../data-source"
-import Card from "../entities/Card.entity"
-import BoardRole from "../entities/BoardRole.entity"
 import AppError from "../errors"
 import { IBoardRole } from "../types/boards.types"
 
@@ -18,7 +16,26 @@ export function authorizeByCardAndBoardRole(roles:IBoardRole[]) {
             .where("c.id = :cardId", { cardId: req.params.cardId })
             .andWhere("u.id = :userId", { userId: res.locals.userId })
             .getRawOne()
-            
+
+        if(!found || !roles.includes(found.br_role!)) 
+            throw new AppError("You don't have authorization for that", 403)
+
+        next()
+    }
+}
+
+export function authorizeByCardListAndBoardRole(roles:IBoardRole[]) {
+    return async (req:Request, res:Response, next:NextFunction) => {
+
+        const found = await AppDataSource
+            .createQueryBuilder("board_roles", "br")
+            .leftJoin("boards", "b", "br.boardId = b.id")
+            .leftJoin("users", "u", "br.userId = u.id")
+            .leftJoin("card_lists", "cl", "cl.boardId = b.id")
+            .where("cl.id = :cardListId", { cardId: req.params.cardListId })
+            .andWhere("u.id = :userId", { userId: res.locals.userId })
+            .getRawOne()
+
         if(!found || !roles.includes(found.br_role!)) 
             throw new AppError("You don't have authorization for that", 403)
 
