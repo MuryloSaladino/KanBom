a2b077ac326930ddb568890fae9f6ed3e0d810dd
MuryloSaladino <murylosaladino@gmail.com>
2024-11-21 19:37:22 -0300

    feat: password validation

--- src/schemas/users.schemas.ts
+++ src/schemas/users.schemas.ts
@@ -1,12 +1,33 @@
 import { z } from "zod";
 
 export const UserSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8),
+    email: z
+        .string()
+        .email("Choose a valid email format"),
+    password: z
+        .string()
+        .min(8, "At least 8 characters")
+        .regex(/(?=.*?[A-Z])/, "At least one uppercase letter")
+        .regex(/(?=.*?[a-z])/, "At least one lowercase letter")
+        .regex(/(?=.*?[0-9])/, "At least one number")
+        .regex(/(?=.*?[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).*$/, { message: "At least one special character" }),
+    confirm: z
+        .string(),
     details: z.object({
-        birthdate: z.string().date(),
-        firstName: z.string().min(2).max(50),
-        lastName: z.string().min(2).max(255),
-        profilePicture: z.string().max(255).nullable().optional()
-    }).strict(),
-}).strict();
+        firstName: z
+            .string()
+            .min(2, "At least 2 characters"),
+        lastName: z 
+            .string()
+            .min(2, "At least 2 characters"),
+        birthdate: z
+            .string(),
+        profilePicture: z
+            .string()
+            .nullable()
+            .optional()
+    }),
+}).refine(({ password, confirm }) => password === confirm, {
+    message: "Password doesn't match",
+    path: ["confirm"],
+})
